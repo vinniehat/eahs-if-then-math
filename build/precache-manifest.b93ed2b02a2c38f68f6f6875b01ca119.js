b77ed1719ec77aa02ed597e93247a81b150f0b23
Vincent Lauro <vinniehat@gmail.com>
2019-09-11 20:04:28 -0400

    Added Build

---
+++ build/precache-manifest.b93ed2b02a2c38f68f6f6875b01ca119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf308829a5d1abbf8b0930b75e1a5409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "528b98daf6e2f30438d1",
+    "url": "/static/css/main.6fb79afc.chunk.css"
+  },
+  {
+    "revision": "cf32a1ef0dcd0c642024",
+    "url": "/static/js/2.ff54d6c6.chunk.js"
+  },
+  {
+    "revision": "528b98daf6e2f30438d1",
+    "url": "/static/js/main.2275ed15.chunk.js"
+  },
+  {
+    "revision": "47cc789d62ebed20a031",
+    "url": "/static/js/runtime~main.568343ec.js"
+  },
+  {
+    "revision": "46f758661588703b190fb4d2aae3847a",
+    "url": "/static/media/nodejs.46f75866.png"
+  }
+]);
